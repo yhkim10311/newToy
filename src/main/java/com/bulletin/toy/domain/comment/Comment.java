1ac8eac6c8ee6dbf4e92dedeb9be1fcd705d1228
김영훈 <yhkim1031@woowahan.com>
2020-10-06 19:26:35 +0900

    Comment added

---
+++ src/main/java/com/bulletin/toy/domain/comment/Comment.java
@@ -0,0 +1,14 @@
+package com.bulletin.toy.domain.comment;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}

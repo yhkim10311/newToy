f19d22493eeb78684b77bb6a2d181be6c6e466a3
김영훈 <yhkim1031@woowahan.com>
2020-10-06 19:25:27 +0900

    IndexController added

---
+++ src/main/java/com/bulletin/toy/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.bulletin.toy.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model){
+
+        return "index";
+    }
+}

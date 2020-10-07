051f97f7c6c32aa6df190442733c82506ed46c3c
김영훈 <yhkim1031@woowahan.com>
2020-10-07 16:35:55 +0900

    Comment 에 필요한 필드값들 추가

--- src/main/java/com/bulletin/toy/domain/comment/Comment.java
+++ src/main/java/com/bulletin/toy/domain/comment/Comment.java
@@ -1,14 +1,47 @@
 package com.bulletin.toy.domain.comment;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.bulletin.toy.domain.BaseTimeEntity;
+import com.bulletin.toy.domain.post.Post;
+import com.bulletin.toy.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public class Comment {
+import javax.persistence.*;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Comment extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private String content;
 
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn
+    private Comment comment;
+
+    @Builder
+    public Comment(String content, User user, Post post, Comment comment){
+        checkArgument(comment == null || comment.getComment()==null);
+
+        this.content = content;
+        this.user = user;
+        this.post = post;
+        this.comment = comment;
+    }
 }

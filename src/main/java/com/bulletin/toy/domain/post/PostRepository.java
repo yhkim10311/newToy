4ee598c2694bfcf07d57e07a04c1ae0222124557
김영훈 <yhkim1031@woowahan.com>
2020-10-07 16:37:56 +0900

    Jpa repository 사용위한 Post,Comment Repository 추가

---
+++ src/main/java/com/bulletin/toy/domain/post/PostRepository.java
@@ -0,0 +1,9 @@
+package com.bulletin.toy.domain.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}

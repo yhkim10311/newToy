166e6475b593358f79a56d5169a2c739fd722b5c
김영훈 <yhkim1031@woowahan.com>
2020-10-08 16:41:16 +0900

    comment optional로 변환

--- src/main/java/com/bulletin/toy/domain/comment/Comment.java
+++ src/main/java/com/bulletin/toy/domain/comment/Comment.java
@@ -3,11 +3,14 @@ package com.bulletin.toy.domain.comment;
 import com.bulletin.toy.domain.BaseTimeEntity;
 import com.bulletin.toy.domain.post.Post;
 import com.bulletin.toy.domain.user.User;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+
+import java.util.Optional;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
@@ -31,17 +34,22 @@ public class Comment extends BaseTimeEntity {
     @JoinColumn(nullable = false)
     private Post post;
 
+    @Getter(AccessLevel.NONE)
     @ManyToOne
     @JoinColumn
     private Comment comment;
 
     @Builder
     public Comment(String content, User user, Post post, Comment comment){
-        checkArgument(comment == null || comment.getComment()==null);
+        checkArgument(comment == null || comment.getComment().orElse(null)==null , "Only 2 depth of comment allowed");
 
         this.content = content;
         this.user = user;
         this.post = post;
         this.comment = comment;
+    }
+
+    public Optional<Comment> getComment(){
+        return Optional.ofNullable(comment);
     }
 }

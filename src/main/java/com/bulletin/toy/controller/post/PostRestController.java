91afbc7df354943e0387a4a716c061aade563ab0
김영훈 <yhkim1031@woowahan.com>
2020-10-08 16:40:44 +0900

    유저 조회 기능 service layer로 내려서 정

--- src/main/java/com/bulletin/toy/controller/post/PostRestController.java
+++ src/main/java/com/bulletin/toy/controller/post/PostRestController.java
@@ -7,8 +7,6 @@ import com.bulletin.toy.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 import static com.bulletin.toy.controller.ApiResult.OK;
 
 @RestController
@@ -18,15 +16,11 @@ public class PostRestController {
 
     private final PostService postService;
 
-    private final UserService userService;
-
     @PostMapping("post")
     public ApiResult<PostDto> posting(@RequestBody PostRequest postRequest){
-        // TODO 임시처리... JWT 개발 후 RequestBody(클래스 필드값) 수정 예정
-        User user = userService.findByEmail(postRequest.getEmail());
 
         return OK(
-                postService.save(postRequest.toEntity(user))
+                postService.save(postRequest)
         );
     }
 
@@ -34,6 +28,13 @@ public class PostRestController {
     public ApiResult<PostDto> delete(@PathVariable Long id){
         return OK(
                 postService.delete(id)
+        );
+    }
+
+    @PutMapping("post/{id}")
+    public ApiResult<PostDto> update(@PathVariable Long id, @RequestBody PostUpdateRequest postUpdateRequest){
+        return OK(
+                postService.update(id, postUpdateRequest)
         );
     }
 }

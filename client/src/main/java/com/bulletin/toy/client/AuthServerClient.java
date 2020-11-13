e74572ecf34df6e082bd8af4eb185edbbb1535f8
김영훈 <yhkim1031@woowahan.com>
2020-11-13 17:10:20 +0900

    auth redirect fixed

--- client/src/main/java/com/bulletin/toy/client/AuthServerClient.java
+++ client/src/main/java/com/bulletin/toy/client/AuthServerClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="authServer", url = "${auth.server-url}")
+@FeignClient(name="authServer", url = "${feign.auth.server-url}")
 public interface AuthServerClient {
 
     @GetMapping("/api/auth/token")
--- client/src/main/java/com/bulletin/toy/client/ResourceServerClient.java
+++ client/src/main/java/com/bulletin/toy/client/ResourceServerClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name="resourceServer", url = "${resource.server-url}")
+@FeignClient(name="resourceServer", url = "${feign.resource.server-url}")
 public interface ResourceServerClient {
 
     @GetMapping("/api/auth/resource")

27b0a16026636f048e0e801e7af54bd821638721
김영훈 <yhkim1031@woowahan.com>
2020-11-13 11:32:16 +0900

    redis setting fixed

--- resource-server/src/main/java/com/resourceser/config/WebMvcConfigure.java
+++ resource-server/src/main/java/com/resourceser/config/WebMvcConfigure.java
@@ -1,11 +1,19 @@
 package com.resourceser.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableJpaAuditing
 public class WebMvcConfigure implements WebMvcConfigurer {
+
+
+    @Bean
+    public ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
 
 }
